87a562c12fc2cc440c5e62e074be2c29a9f59474
Tribo310 <monhochir0131@gmail.com>
2026-04-13 23:24:30 +0800

    first commmit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Marlaa from './src/marlaa/page';
+import Baaska from './src/baaska/page';
+import Aminaa from './src/aminaa/page'
+
+
+export default function Home() {
+  return (
+    <>
+      <div>We con do it</div>
+      <Aminaa />
+      <Baaska />
+      <Marlaa />
+    </>
+  );
+}
